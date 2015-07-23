877e1c86639ada52772cef0887907eea93b9a2e8
Stefan Bruvik <stefan@colours.no>
2015-07-23 16:50:10 +0200

    Add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var browserSync = require('browser-sync').create();
 
-gulp.task('default', function() {
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    browser: ['Google Chrome Canary'],
+    port: 8000,
+    server: {
+      baseDir: "./"
+    }
+  });
+});
+
+gulp.task('babel', function() {
   return gulp.src('src/snake.js')
     .pipe(babel())
     .pipe(gulp.dest('dist'));
 });
+
+gulp.task('default', ['babel', 'browser-sync']);
