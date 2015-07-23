30cd73af4cca20ccb22e9731102ecf38f9f0bb12
Stefan Bruvik <stefan@colours.no>
2015-07-23 21:03:21 +0200

    sass

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
 import game from './game.js';
-import settings from './settings.js';
 
 game.start();
-
-var snake = game.board.snake,
-  keycodes = settings.keycodes,
-  directions = settings.directions;
