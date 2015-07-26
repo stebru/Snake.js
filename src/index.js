4bb82b81495630ca1bf26bc6fe9c9d60a3372488
Stefan Bruvik <stefan@colours.no>
2015-07-26 19:59:41 +0200

    Add service worker for offline support

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import game from './game.js';
 
 game.start();
+
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('../sw.js').then(function(ok) {
+		console.log('SW registered', ok);
+	}).catch(function(err) {
+		console.log('SW error', err);
+	});
+}
---
+++ sw.js
@@ -0,0 +1,60 @@
+var CACHE_NAME = 'snake-cache-v2';
+var urls = [
+  'index.html',
+  '/dist/bundle.js',
+  '/bower_components/normalize.css/normalize.css',
+  '/dist/snake.css'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(function() {
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urls);
+    });
+  });
+});
+
+self.addEventListener('activate', function(event) {
+
+  var cacheWhitelist = ['snake-cache-v2'];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (~~cacheWhitelist.indexOf(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(response) {
+      if (response) {
+        return response;
+      }
+
+      var fetchRequest = event.request.clone();
+
+      return fetch(fetchRequest).then(function(response) {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+
+        var responseToCache = response.clone();
+
+        caches.open(CACHE_NAME)
+          .then(function(cache) {
+            cache.put(event.request, responseToCache);
+          });
+
+        return reponse;
+      });
+    })
+  );
+});
