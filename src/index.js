b37b0787e4e9a9bcf1eec278c0fdcf58a59bf620
Stefan Bruvik <stefan@colours.no>
2015-07-26 21:56:56 +0200

    fix path

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import game from './game.js';
 game.start();
 
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('/sw.js').then(function(ok) {
+	navigator.serviceWorker.register('./sw.js').then(function(ok) {
 		console.log('SW registered', ok);
 	}).catch(function(err) {
 		console.log('SW error', err);
